5afc2b8b6b9505c1e97166f9c1094f81b79380ef
acanessa <augustocanessa@gmail.com>
2016-10-05 03:01:08 +0000

    yeah

--- test/test.js
+++ test/test.js
@@ -1,59 +1,59 @@
 var supertest = require("supertest");
 var should = require("should");
-
-// This agent refers to PORT where the program is running.
-
+var assert = require('assert');
 var server = supertest.agent("http://localhost:7474");
 
-// UNIT test begin
 
-//describe("SAMPLE unit test",function(){
-//
-//  before(function(done) {
-//    server
-//      .post('/')
-//      .send({ username: 'acanessa', password: 'h2vgvj4dd9!' })
-//      .end(function(err, res) {
-//        token = res.body.token; // Or something
-//        done();
-//      });
-//  });
-//    
-//  // #1 should return home page
-//  it("should return home page",function(done){
-//    // calling home page
-//    server
-//    .get("/")
-//    .expect("Content-type",/text/)
-//    .expect(200) // THis is HTTP response
-//    .end(function(err,res){
-//      // HTTP status should be 200
-//      res.status.should.equal(200);
-//      done();
-//    });
-//  });
-//
-//});
+describe ('Login',function(){
+var token = null;
+    before(function(done) {
+        server
+        .post('/login')
+        .send({ username: 'acanessa', password: 'XXXXX' })
+        .end(function(err, res) {
+            token = res.body.token; // Or something
+            done();
+        });
+    });
+    
+    describe('Token', function() {  
+        it('should not be null', function(done) { 
+           token.should.not.equal(null);
+            done();
+        });
+    });
 
-
-describe('Token auth', function() {
-
-  var token = null;
-
-  before(function(done) {
-    server
-      .post('/login')
-      .send({ username: 'acanessa', password: 'h2vgvj4dd9!' })
-      .end(function(err, res) {
-        token = res.body.token; // Or something
-        done();
-      });
-  });
-
-  it('should get a valid token from server', function(done) { 
-    server
-      .get('/')
-      .set('Authorization', token)
-      .expect(200, done);
-  });
+    describe('After posting username and password', function() {  
+        it('should get a valid token from server', function(done) { 
+        server
+          .get('/')
+          .set('Authorization', token)
+          .expect(200, done);
+        });
+    });
+    
+    describe ('After getting a token',function(){
+        it('should be able to GET request /', function(done) { 
+            server
+            .get('/')
+            .set('Authorization', token)
+            .expect(200, done);
+        });    
+    });    
+    
+    describe ('Home page',function(){
+        it('should return "Hello World!!!!!!!!!!"', function(done) { 
+            server
+            .get('/')
+            .set('Authorization', token)
+            .expect('Hello World!!!!!!!!!!', done);
+        });    
+    });    
+    
+    
 });
+
+
+
+ 
+
